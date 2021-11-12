b94232fc4f75d072f8331644c66620b5bb2bb6c4
Edvin Galoyan <edvin.galoyan@gmail.com>
2021-11-12 17:06:42 +0400

    first-commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/learning-proj'),
+      dir: require('path').join(__dirname, './coverage/learning-project'),
       subdir: '.',
       reporters: [
         { type: 'html' },
